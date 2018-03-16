90fd9202efb5ae5c95c6ee65ccbbea548674f8fb
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-17 01:24:00 +0300

    feat: Добавил отображение видео с веб-камеры в тег video

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('It\'s work');
+const Player = document.getElementById('Player');
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  .then(stream => {
+    try {
+      Player.srcObject = stream;
+    } catch (error) {
+      Player.src = URL.createObjectURL(stream);
+    }
+  });
